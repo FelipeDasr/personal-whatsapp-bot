abc535842cb88476bf22acbedd57bdd4bee1d9c6
Felipe dos anjos <40957376+FelipeDosAnjos@users.noreply.github.com>
2021-08-13 21:24:24 -0300

    Update options.js

--- src/options.js
+++ src/options.js
@@ -1,7 +1,11 @@
 module.exports = {
-    useChrome: true,
-    autoRefresh:true,
-    cacheEnabled:false,
-    sessionId: 'hr',
-    qrTimeout: 0
+  sessionId: "BOT_JOSELITO",
+  authTimeout: 60, //wait only 60 seconds to get a connection with the host account device
+  blockCrashLogs: true,
+  disableSpins: true,
+  headless: true,
+  hostNotificationLang: 'PT_BR',
+  logConsole: false,
+  popup: true,
+  qrTimeout: 0, //0 means it will wait forever for you to scan the qr code
 };
